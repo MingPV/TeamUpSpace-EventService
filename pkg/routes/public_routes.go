1f934a6fb8e06197a15a7f995ccd11f21345d1f1
MingPV <jeung.pavee@gmail.com>
2025-09-01 23:22:51 +0700

    remove user

--- pkg/routes/public_routes.go
+++ pkg/routes/public_routes.go
@@ -8,11 +8,6 @@ import (
 	orderHandler "github.com/MingPV/EventService/internal/order/handler/rest"
 	orderRepository "github.com/MingPV/EventService/internal/order/repository"
 	orderUseCase "github.com/MingPV/EventService/internal/order/usecase"
-
-	// User
-	userHandler "github.com/MingPV/EventService/internal/user/handler/rest"
-	userRepository "github.com/MingPV/EventService/internal/user/repository"
-	userUseCase "github.com/MingPV/EventService/internal/user/usecase"
 )
 
 func RegisterPublicRoutes(app fiber.Router, db *gorm.DB) {
@@ -26,24 +21,7 @@ func RegisterPublicRoutes(app fiber.Router, db *gorm.DB) {
 	orderService := orderUseCase.NewOrderService(orderRepo)
 	orderHandler := orderHandler.NewHttpOrderHandler(orderService)
 
-	// User
-	userRepo := userRepository.NewGormUserRepository(db)
-	EventService := userUseCase.NewEventService(userRepo)
-	userHandler := userHandler.NewHttpUserHandler(EventService)
-
 	// === Public Routes ===
-
-	// Auth routes (separated from /users)
-	authGroup := api.Group("/auth")
-	authGroup.Post("/signup", userHandler.Register)
-	authGroup.Post("/signin", userHandler.Login)
-
-	// User routes
-	userGroup := api.Group("/users")
-	userGroup.Get("/", userHandler.FindAllUsers)
-	userGroup.Get("/:id", userHandler.FindUserByID)
-	userGroup.Patch("/:id", userHandler.PatchUser)
-	userGroup.Delete("/:id", userHandler.DeleteUser)
 
 	// Order routes
 	orderGroup := api.Group("/orders")
